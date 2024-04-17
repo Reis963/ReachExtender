dfeb1e1a819c16f27b964441503482c9fe35fe30
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-04-16 20:51:00 -0400

    Added references to repo and fixed plugin GUID

--- ReachExtender/ReachExtender.cs
+++ ReachExtender/ReachExtender.cs
@@ -2,7 +2,7 @@
 using BepInEx.Configuration;
 
 namespace ReachExtender {
-    [BepInPlugin("Mattdokn", "ReachExtender", "1.0.0")]
+    [BepInPlugin("Mattdokn.ReachExtender", "ReachExtender", "1.0.1")]
     public class ReachExtenderPlugin : BaseUnityPlugin {
 
         public static ConfigEntry<float> InteractionDistance;
@@ -10,7 +10,6 @@ namespace ReachExtender {
 
 
         void Awake() {
-
             InteractionDistance = Config.Bind("Reach Distances", "Loot/Container Interaction Distance", 2f, new ConfigDescription("Default Value is 1.3", new AcceptableValueRange<float>(0f, 20f)));
             DoorInteractionDistance = Config.Bind("Reach Distances", "Door Interaction Distance", 1f, new ConfigDescription("Default Value is 1", new AcceptableValueRange<float>(0f, 5f)));
 
