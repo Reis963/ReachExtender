8f8ed294fc2b4de96c81d6e9b449770fde4a5c46
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-10-02 21:57:22 -0400

    No more ball :(

--- ReachExtender/Patches.cs
+++ ReachExtender/Patches.cs
@@ -11,29 +11,13 @@ namespace ReachExtender.Patches {
         [PatchPrefix]
         private static bool PatchPrefix(GameWorld __instance, ref GameObject __result, Ray ray, out RaycastHit hit) {
             __result = null;
-
-            //GameObject gameObject = EFTPhysicsClass.Raycast(ray, out hit, Mathf.Max(EFTHardSettings.Instance.LOOT_RAYCAST_DISTANCE, EFTHardSettings.Instance.PLAYER_RAYCAST_DISTANCE + EFTHardSettings.Instance.BEHIND_CAST), GameWorld.InteractiveLootMaskWPlayer) ? hit.collider.gameObject : null;
-            GameObject gameObject = EFTPhysicsClass.SphereCast(ray, ReachExtenderPlugin.SphereCastRadius.Value, out hit, Mathf.Max(EFTHardSettings.Instance.LOOT_RAYCAST_DISTANCE, EFTHardSettings.Instance.PLAYER_RAYCAST_DISTANCE + EFTHardSettings.Instance.BEHIND_CAST), GameWorld.InteractiveLootMaskWPlayer) ? hit.collider.gameObject : null;
-            if (ReachExtenderPlugin.ShowSphereCastDebug.Value) {
-                RaycastHit balls;
-                GameObject ball = EFTPhysicsClass.SphereCast(ray, ReachExtenderPlugin.SphereCastRadius.Value, out balls, Mathf.Infinity, GameWorld.LootMaskObstruction) ? balls.collider.gameObject : null;
-                // Get/create sphere and move/scale it to where we want
-                if (ReachExtenderPlugin.Sphere == null) {
-                    ReachExtenderPlugin.Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    GameObject.Destroy(ReachExtenderPlugin.Sphere.GetComponent<SphereCollider>());
-                    Color col = Color.red;
-                    col.a = 0.5f;
-                    Material newMat = new Material(Shader.Find("Legacy Shaders/Transparent/Diffuse"));
-                    newMat.SetColor("_Color", col);
-                    ReachExtenderPlugin.Sphere.GetComponent<MeshRenderer>().sharedMaterial = newMat;
-
-                }
-                
-                if (ball) {
-                    ReachExtenderPlugin.Sphere.transform.position = ray.GetPoint(balls.distance);
-                    ReachExtenderPlugin.Sphere.transform.localScale = Vector3.one * ReachExtenderPlugin.SphereCastRadius.Value;
-                }
+            GameObject gameObject = null;
+            if (ReachExtenderPlugin.SphereCastRadius.Value == 0f) {
+                gameObject = EFTPhysicsClass.Raycast(ray, out hit, Mathf.Max(EFTHardSettings.Instance.LOOT_RAYCAST_DISTANCE, EFTHardSettings.Instance.PLAYER_RAYCAST_DISTANCE + EFTHardSettings.Instance.BEHIND_CAST), GameWorld.InteractiveLootMaskWPlayer) ? hit.collider.gameObject : null;
+            } else {
+                gameObject = EFTPhysicsClass.SphereCast(ray, ReachExtenderPlugin.SphereCastRadius.Value, out hit, Mathf.Max(EFTHardSettings.Instance.LOOT_RAYCAST_DISTANCE, EFTHardSettings.Instance.PLAYER_RAYCAST_DISTANCE + EFTHardSettings.Instance.BEHIND_CAST), GameWorld.InteractiveLootMaskWPlayer) ? hit.collider.gameObject : null;
             }
+
             if (gameObject) {
                 // Get raycast to obstruction
                 RaycastHit obstructionHit;
