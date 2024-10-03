799545d151dbe60a0f4924a84d48a333b307edba
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-10-02 21:49:45 -0400

    The ball

--- ReachExtender/Patches.cs
+++ ReachExtender/Patches.cs
@@ -12,7 +12,28 @@ namespace ReachExtender.Patches {
         private static bool PatchPrefix(GameWorld __instance, ref GameObject __result, Ray ray, out RaycastHit hit) {
             __result = null;
 
-            GameObject gameObject = EFTPhysicsClass.Raycast(ray, out hit, Mathf.Max(EFTHardSettings.Instance.LOOT_RAYCAST_DISTANCE, EFTHardSettings.Instance.PLAYER_RAYCAST_DISTANCE + EFTHardSettings.Instance.BEHIND_CAST), GameWorld.InteractiveLootMaskWPlayer) ? hit.collider.gameObject : null;
+            //GameObject gameObject = EFTPhysicsClass.Raycast(ray, out hit, Mathf.Max(EFTHardSettings.Instance.LOOT_RAYCAST_DISTANCE, EFTHardSettings.Instance.PLAYER_RAYCAST_DISTANCE + EFTHardSettings.Instance.BEHIND_CAST), GameWorld.InteractiveLootMaskWPlayer) ? hit.collider.gameObject : null;
+            GameObject gameObject = EFTPhysicsClass.SphereCast(ray, ReachExtenderPlugin.SphereCastRadius.Value, out hit, Mathf.Max(EFTHardSettings.Instance.LOOT_RAYCAST_DISTANCE, EFTHardSettings.Instance.PLAYER_RAYCAST_DISTANCE + EFTHardSettings.Instance.BEHIND_CAST), GameWorld.InteractiveLootMaskWPlayer) ? hit.collider.gameObject : null;
+            if (ReachExtenderPlugin.ShowSphereCastDebug.Value) {
+                RaycastHit balls;
+                GameObject ball = EFTPhysicsClass.SphereCast(ray, ReachExtenderPlugin.SphereCastRadius.Value, out balls, Mathf.Infinity, GameWorld.LootMaskObstruction) ? balls.collider.gameObject : null;
+                // Get/create sphere and move/scale it to where we want
+                if (ReachExtenderPlugin.Sphere == null) {
+                    ReachExtenderPlugin.Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    GameObject.Destroy(ReachExtenderPlugin.Sphere.GetComponent<SphereCollider>());
+                    Color col = Color.red;
+                    col.a = 0.5f;
+                    Material newMat = new Material(Shader.Find("Legacy Shaders/Transparent/Diffuse"));
+                    newMat.SetColor("_Color", col);
+                    ReachExtenderPlugin.Sphere.GetComponent<MeshRenderer>().sharedMaterial = newMat;
+
+                }
+                
+                if (ball) {
+                    ReachExtenderPlugin.Sphere.transform.position = ray.GetPoint(balls.distance);
+                    ReachExtenderPlugin.Sphere.transform.localScale = Vector3.one * ReachExtenderPlugin.SphereCastRadius.Value;
+                }
+            }
             if (gameObject) {
                 // Get raycast to obstruction
                 RaycastHit obstructionHit;
--- ReachExtender/ReachExtender.cs
+++ ReachExtender/ReachExtender.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using ReachExtender.Patches;
+using UnityEngine;
 
 namespace ReachExtender {
     [BepInPlugin("Mattdokn.ReachExtender", "ReachExtender", "1.2.0")]
@@ -11,11 +12,17 @@ namespace ReachExtender {
         public static ConfigEntry<float> BelowFloorMaxLootDistance;
         public static float DistanceFromWallSquared;
 
+        public static ConfigEntry<float> SphereCastRadius;
+        public static ConfigEntry<bool> ShowSphereCastDebug;
+
+        public static GameObject Sphere;
 
         void Awake() {
             InteractionDistance = Config.Bind("Loot Distances", "Loot/Container Interaction Distance", 2f, new ConfigDescription("Default Value is 1.3", new AcceptableValueRange<float>(0f, 20f)));
             DoorInteractionDistance = Config.Bind("Door Distances", "Door Interaction Distance", 1f, new ConfigDescription("Default Value is 1", new AcceptableValueRange<float>(0f, 5f)));
             BelowFloorMaxLootDistance = Config.Bind("Loot Distances", "How far to reach below floors to loot.", 0.75f, new ConfigDescription("Set to 0 to disable feature.", new AcceptableValueRange<float>(0f, 5f)));
+            SphereCastRadius = Config.Bind("Loot Distances", "Sphere Cast Radius.", 0.1f, new ConfigDescription("Set to 0 to disable feature.", new AcceptableValueRange<float>(0f, 5f)));
+            ShowSphereCastDebug = Config.Bind("Loot Distances", "Show debug sphere.", false, new ConfigDescription("Shows the radius of the sphere used for casting."));
 
             EFTHardSettings.Instance.LOOT_RAYCAST_DISTANCE = InteractionDistance.Value;
             EFTHardSettings.Instance.DOOR_RAYCAST_DISTANCE = DoorInteractionDistance.Value;
